c239e3f06f34abd114230ae0d9224c9e6b19ea1c
LarryKoo <larrykoo@126.com>
2019-06-20 17:29:30 +0800

    new: learning-ch4 运算符

--- src/ch3/type_test/type_test.go
+++ src/ch3/type_test/type_test.go
@@ -19,8 +19,31 @@ func TestPoint(t *testing.T) {
 	t.Logf("%T %T", a, aPtr)
 }
 
+/**
+Go 对没有赋值的变量会自动进行赋值成该变量类型的 "零值"
+===================
+bool : false
+integer : 0
+float : 0.0
+string : ""
+pointer, function, interface, slice, channel, map : nil
+*/
 func TestString(t *testing.T) {
 	var s string
 
 	t.Logf("*%s*", s)
+}
+
+/**
+以下单元测试证明了：
+Go 赋值是：值传递，对两个变量赋值是，是开创一个新的内存空间，然后拷贝所有元素
+*/
+func TestPoint2(t *testing.T) {
+	a := [...]int{1, 2, 3}
+	b := a
+
+	b[0] = 9
+
+	t.Log(a, b)
+
 }
---
+++ src/ch4/operator_test/operator_test.go
@@ -0,0 +1,39 @@
+package operator_test
+
+import "testing"
+
+/**
+ * 数组的比较，必须保证两个数组的维数相同才能比较
+ */
+func TestCompareArray(t *testing.T) {
+	a := [...]int{1, 2, 3, 4}
+	b := [...]int{1, 3, 2, 4}
+	//c := [...]int{1, 2, 3}
+	d := [...]int{1, 2, 3, 4}
+
+	t.Log(a == b)
+	//t.Log(a == c)
+	t.Log(a == d)
+}
+
+const (
+	Readable = 1 << iota
+	Writable
+	Executable
+)
+
+// 测试对变量的置零操作符 &^
+func TestBitClear(t *testing.T) {
+
+	// 结果是 1 2 4，貌似刚好对应上 Linux 上的[rwx] read=1, write=2, execution=4
+	t.Log(Readable, Writable, Executable)
+
+	a := 7            // 0111	 7 包含读写执行全部权限
+	a = a &^ Readable // 清楚 a 的读权限
+
+	t.Log("a=", a)
+
+	t.Log("a&Readable=", a&Readable, "a&Writable=", a&Writable, "a&Executable=", a&Executable)
+	t.Log(a&Readable == Readable, a&Writable == Writable, a&Executable == Executable)
+
+}
